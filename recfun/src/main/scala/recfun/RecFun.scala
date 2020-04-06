02677aa39acfecba2b8a84649aae75264ae4e091
sid lin <j611062000@gmail.com>
2020-04-06 22:43:06 +0800

    finished week 1

--- recfun/src/main/scala/recfun/RecFun.scala
+++ recfun/src/main/scala/recfun/RecFun.scala
@@ -25,10 +25,12 @@ object RecFun extends RecFunInterface {
   def balance(chars: List[Char]): Boolean = {
     def loop(leftPar: Int, chars: List[Char]): Boolean = {
       if (chars.isEmpty) leftPar == 0
-      chars.head match {
-        case ')' => loop(leftPar - 1, chars.tail)
-        case '(' => loop(leftPar + 1, chars.tail)
-        case _ => loop(leftPar, chars.tail)
+      else {
+        chars.head match {
+          case ')' => if (leftPar>= 1)  loop(leftPar - 1, chars.tail) else false
+          case '(' => loop(leftPar + 1, chars.tail)
+          case _ => loop(leftPar, chars.tail)
+        }
       }
     }
     loop(0, chars)
@@ -37,5 +39,11 @@ object RecFun extends RecFunInterface {
   /**
    * Exercise 3
    */
-  def countChange(money: Int, coins: List[Int]): Int = 1
+  def countChange(money: Int, coins: List[Int]): Int = {
+    if (money == 0) 1
+    else if (money < 0) 0
+    else if (money >0 && coins.isEmpty) 0
+    else countChange(money - coins.head, coins) + countChange(money, coins.tail)
+
+  }
 }
